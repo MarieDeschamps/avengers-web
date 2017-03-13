98eaa05a178aa986f21f7579328463377f2acc28
MarieDeschamps <marie.deschamps.1991@gmail.com>
2017-03-13 15:59:37 +0100

    first commit

---
+++ src/io/avengers/ws/MarvelApplication.java
@@ -0,0 +1,9 @@
+package io.avengers.ws;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("marvel")
+public class MarvelApplication extends Application{
+
+}
