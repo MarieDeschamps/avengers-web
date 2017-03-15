dcb1cfc42e1aa2f66dda62fbb0fdb7a55b37d97e
MarieDeschamps <marie.deschamps.1991@gmail.com>
2017-03-15 10:05:17 +0100

    HeroResource

--- src/io/avengers/ws/HeroResource.java
+++ src/io/avengers/ws/HeroResource.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Set;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import io.avengers.domain.Hero;
 import io.avengers.service.HeroService;
@@ -27,4 +29,17 @@ public class HeroResource {
 		HeroService hService = new HeroService();
 		return hService.findHero(id);
 	}
+	
+	
+	@POST
+	public Response createHero(Hero hero){
+		if(hero==null || hero.getAlias().isEmpty()){
+			return Response.status(406).entity("\"Empty hero\"").build();
+		}
+		
+		new HeroService().createHero(hero);
+		
+		return Response.status(201).entity("\""+hero.getAlias()+"\"").build();
+	}
+		
 }
