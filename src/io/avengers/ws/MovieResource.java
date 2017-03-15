3303cd1564d61e0d0965b164796095581c592d9f
raqueldj <raquel.diasjales@gmail.com>
2017-03-15 11:01:26 +0100

    MovieResource create movie

--- src/io/avengers/ws/MovieResource.java
+++ src/io/avengers/ws/MovieResource.java
@@ -3,10 +3,12 @@ package io.avengers.ws;
 import java.util.Set;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import io.avengers.domain.Movie;
 import io.avengers.service.MovieService;
@@ -25,5 +27,15 @@ public class MovieResource {
 	public Movie getMovieById(@PathParam("id") int id){
 		MovieService mService = new MovieService();
 		return mService.findMovie(id);
+	}
+	@POST
+	public Response createMovie(Movie movie){
+		if(movie==null || movie.getMovie_title().isEmpty()){
+			return Response.status(406).entity("\"Empty movie\"").build();
+		}
+		
+		new MovieService().createMovie(movie);
+		
+		return Response.status(201).entity("\""+movie.getMovie_title()+"\"").build();
 	}
 }
